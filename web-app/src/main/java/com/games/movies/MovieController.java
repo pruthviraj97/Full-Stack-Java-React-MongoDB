405d2da1160f7968db19d4378260548af8d065ed
pruthviraj97 <pruthviraj2828@gmail.com>
2024-08-07 04:40:08 -0700

    pushing backend code

---
+++ web-app/src/main/java/com/games/movies/MovieController.java
@@ -0,0 +1,28 @@
+package com.games.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getwAllMovies() {
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
